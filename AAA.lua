0dbfc21c2911230dc71201fc7ec524a0355b685c
leo9515 <382152174@qq.com>
2016-10-08 01:12:23 +0800

    delete

--- AAA.lua
+++
@@ -1,8 +0,0 @@
-function readScript(file)
-    local f = io.open(file, "rb")
-    local content = f:read("*all")
-    f:close()
-return content
-end
-assert(load(readScript(LIB_PATH.."AAA.luac"), nil, "bt", _ENV))()
-
